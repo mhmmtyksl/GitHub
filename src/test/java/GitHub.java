887db936084ea72554e76a0b9e2ce2d61fe05746
mhmmtyksl <mhmmt.yksl1@gmail.com>
2021-12-09 20:56:56 +0100

    commit ile

--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -1,5 +1,9 @@
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hallo World");
+        System.out.println("Master a eklendi");
+        System.out.println("muhammet te iken yazildi");
+        System.out.println("muhammet te 2. deneme");
+
     }
 }
